ffdbe86734c21f51deb17e36d84ecd7142a1cec8
Jacek Pietrzyk <jacek.pietrzyk@outlook.com>
2015-09-10 12:27:08 +0200

    Createing scaffold for sinatra application

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+# Encoding: utf-8
+
+require 'rack/test'
+require 'rspec'
+require 'rspec/mocks'
+ENV['RACK_ENV'] = 'test'
+require File.expand_path '../../application.rb', __FILE__
+
+module RSpecConfig
+  include Rack::Test::Methods
+  def application() Sinatra::Application end
+end
+
+RSpec.configure { |c| c.include RSpecConfig }
