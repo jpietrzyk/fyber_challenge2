dc9b86e368e6c36f924f16bccb05eaaf78649ccf
jacek.pietrzyk <jacek.pietrzyk@outlook.com>
2015-09-15 12:38:12 +0200

    Some code fixes

--- lib/jobs/package_job.rb
+++ lib/jobs/package_job.rb
@@ -7,17 +7,20 @@ module Jobs
         packages.each do |package|
           name = package['Package']
           version = package['Version']
-
-          pkg = Package.where(name: name).first
-          if pkg && !pkg.package_versions.where(version: version).any?
-            # package version doesn't exist - so should create
-            get_info_from_package(name, version, pkg, settings)
-          else
-            # package doesn't exist
-            pkg = Package.create(name: name)
-            get_info_from_package(name, version, pkg, settings)
-          end
+          import_package(name, version, settings)
         end
+      end
+    end
+
+    def self.import_package(name, version, settings)
+      pkg = Package.where(name: name).first
+      if pkg && !pkg.package_versions.where(version: version).any?
+        # create version
+        get_info_from_package(name, version, pkg, settings)
+      elsif !pkg
+        # package doesn't exist
+        pkg = Package.create(name: name)
+        get_info_from_package(name, version, pkg, settings)
       end
     end
   end
