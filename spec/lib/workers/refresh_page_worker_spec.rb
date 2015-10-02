976a2db52ec629bc367f9c35bf2792020a9f05dc
jacek.pietrzyk <jacek.pietrzyk@outlook.com>
2015-10-02 12:46:34 +0200

    Fixed worker spec

--- spec/lib/workers/refresh_page_worker_spec.rb
+++ spec/lib/workers/refresh_page_worker_spec.rb
@@ -3,16 +3,13 @@ require File.expand_path '../../../spec_helper.rb', __FILE__
 describe RefreshPackageWorker do
   describe 'scheduling' do
     it 'will be scheduled daily' do
+      Timecop.freeze(Time.now.beginning_of_day)
       valid, invalid = times
 
       expect(next_occurrence).to eq(valid)
       expect(next_occurrence).not_to eq(invalid)
 
-      Timecop.freeze(Time.now.beginning_of_day + 1.day + 13.hours)
-
-      # Testing the past time is no longer valid
-      expect(next_occurrence).not_to eq(valid)
-
+      Timecop.freeze(Time.now.beginning_of_day + 13.hours)
       valid, invalid = times(1.day)
 
       expect(next_occurrence).to eq(valid)
@@ -24,7 +21,7 @@ describe RefreshPackageWorker do
     RefreshPackageWorker.schedule.next_occurrence
   end
 
-  def times(offset = 1.day)
+  def times(offset = 0.day)
     valid = Time.now.beginning_of_day + 12.hours + offset
     invalid = valid + 1.minute
 
